43d6ac943d31f0310dde4ef2c2d00c085cc3d179
GuilhermeCarezia <barbacrossfiteiro@gmail.com>
2025-08-25 21:48:08 -0300

    refatoração: substitui ComponenteExemplo por RouteApp na estrutura do App

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 
 import "font-awesome/css/font-awesome.css";
 import "react-toastify/dist/ReactToastify.css";
-import { ComponenteExemplo } from "./componentes/ComponenteExemplo";
+import { MinhaNavBar } from "./componentes/NavBar";
+import { RouteApp } from "./route";
+
 
 function App() {
   return (
     <div className="App">
-      <h1>APP</h1>
-      <ComponenteExemplo />
+     
+      <RouteApp/>
     </div>
   );
 }
